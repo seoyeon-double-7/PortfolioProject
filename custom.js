279f040cf61288302b421a0fe3133aa7c02ef75e
SEOYEON BAE <83990943+seoyeon-double-7@users.noreply.github.com>
2022-06-04 16:00:48 +0900

    Add files via upload

--- custom.js
+++ custom.js
@@ -1,33 +1,31 @@
 $(function(){
-  // Slick Slider - Welcome
   /* Slick Slider - Welcome */
-$('.slideshow').slick({
-	infinite: true, // 무한반복
-	dots: false, // 동그라미 네비게이션 사용안함
-	autoplay: true, // 자동 재생
-	autoplaySpeed: 2000, // 자동 재생 시 머무는 시간
-	fade: true, // 크로스 페이드 트랜지션
-	speed: 1000, // 자동 재생 트랜지션 시간
-	pauseOnHover: false // 마우스 오버될 때 멈춤 사용안함
-});
+  $('.slideshow').slick({
+    infinite: true,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    speed: 1000,
+    pauseOnHover: false
+  });
 
-/* TypeIt */
-/* TypeIt - Welcome */
-$('#typing').typeIt({
-	strings: ["Business strategy.", "Innovation Plan.", "Creative Idea."], // 타이핑 텍스트 입력
-	speed: 100, // 알파벳 타이핑 속도
-	autoStart: true, // 자동 재생 사용
-	breakLines: false, // 줄 바꿈 사용안함
-});
+  /* TypeIt - Welcome */
+  $('#typing').typeIt({
+    strings: ["Business strategy.", "Innovation Plan.", "Creative Idea."],
+    speed: 100,
+    autoStart: true,
+    breakLines: false,
+  });
 })
 
-// faq Accordion
+/* Faq Accordion */
 $(function(){
-	$('.faq-desc').eq(0).show()
-	$('.faq-title').click(function(){
-		$(this).next().stop().slideDown()
-		$(this).parent().siblings('.faq-item').children('.faq-desc').slideUp()
-		$(this).parent().addClass('active')
-		$(this).parent().siblings().removeClass('active')
-	})
+  $('.faq-desc').eq(0).show()
+  $('.faq-title').click(function(){
+    $(this).next().stop().slideDown()
+    $(this).parent().siblings().children('.faq-desc').stop().slideUp()
+    $(this).parent().addClass('active')
+    $(this).parent().siblings().removeClass('active')
+  })
 })
